dbf85c7d3ad74a3f1cc6a391dce36c343833f467
mnurs <106974842+HilmiMuzh@users.noreply.github.com>
2022-06-27 21:56:23 +0700

    membuat dan mengubah tampilan member card

--- lib/member/card.dart
+++ lib/member/card.dart
@@ -15,136 +15,45 @@ class _MemberCardState extends State<MemberCard> {
   @override
   void initState(){
     super.initState();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-    ]);
   }
 
   @override
   dispose(){
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
     super.dispose();
   }
   Widget build(BuildContext context) {
-    var regis = "";
-    try{
-      DateTime parseDate = new DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse("05-06-2022");
-      var inputDate = DateTime.parse(parseDate.toString());
-      var outputFormat = DateFormat('MM/yy');
-      regis = outputFormat.format(inputDate);
-    }catch(ex){
-
-    }
-
     return Scaffold(
         appBar:  ReusableAppbar.getAppBar("Klinikki SkinCare",Colors.pink[200],context),
-        body: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              print('test' + constraints.constrainHeight().toString());
-              return  Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image:  AssetImage("img/example_card12.png"),
-                    fit: BoxFit.fitHeight,
-                  ),
+        body: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image:  AssetImage("img/example_card12.png"),
+                  fit: BoxFit.fitHeight,
                 ),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      top: constraints.constrainHeight() / 2 + 30,
-                      left: constraints.constrainWidth() / 4 - 0,
-                      height:250,
-                      width: 250,
-                      child: Container(
-                        child: Text(
-                          'SL - M0001',
-                          style: TextStyle(
-                              color : Color(0xFFffdb88),
-                              fontSize: 25,
-                              fontFamily: 'OCRAStd'
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: constraints.constrainHeight() / 2 + 70,
-                      left: constraints.constrainWidth() / 4 - 0,
-                      height:250,
-                      width: 250,
-                      child: Container(
-                        child: Text(
-                          'Executive Number',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontStyle: FontStyle.italic,
-                              fontFamily: 'Montserrat',
-                              fontSize: 12
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: constraints.constrainHeight() / 2 + 90,
-                      left: constraints.constrainWidth() / 4 - 0,
-                      height:250,
-                      width: 250,
-                      child: Container(
-                        child: Text(
-                          'Muhammad Nur Syaifullah',
-                          style: TextStyle(
-                              color : Color(0xFFffdb88),
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: constraints.constrainHeight() / 2 + 70,
-                      left: constraints.constrainWidth() / 2 + 90,
-                      height:250,
-                      width: 250,
-                      child: Container(
-                        child: Text(
-                          'Glowing Since',
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              color: Colors.white,
-                              fontSize: 14
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: constraints.constrainHeight() / 2 + 90,
-                      left: constraints.constrainWidth() / 2 + 110,
-                      height:250,
-                      width: 250,
-                      child: Container(
-                        child: Text(
-                          '${regis.toString()}',
-                          style: TextStyle(
-                              color : Color(0xFFffdb88),
-                              // fontWeight: FontWeight.w900,
-                              fontFamily: 'OCRAStd',
-                              fontSize: 12
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10.0),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image:  AssetImage("img/example_card13.png"),
+                  fit: BoxFit.fitHeight,
                 ),
-              );
-            })
+              ),
+            )
+          ],
+        )
     );
   }
 }
